211df08b1d4a846d9851dbd79035b1aecad50170
Christian Dullweber <xchrdw@gmail.com>
2013-12-01 22:02:25 +0100

    small improvement

--- parser/TableGenerator.py
+++ parser/TableGenerator.py
@@ -2,22 +2,22 @@ import argparse
 import CsvReader
 from collections import defaultdict
 
+
 def computeTable(f):
-    table = defaultdict(int)
+    table = {}
     with open(f, "r") as f:
         for entity, claims in CsvReader.read_csv(f):
-            for claim1 in claims:
-                pid1 = claim1[0]
+            for pid1, datatype, value in claims:
                 if not pid1 in table:
                     table[pid1] = defaultdict(int)
-                    table[pid1]["type"] = claim1[1]
+                    table[pid1]["type"] = datatype
                 table[pid1]["appearances"] += 1
-                for claim2 in claims:
-                    pid2 = claim2[0]
+                for pid2, _, _ in claims:
                     if pid1 != pid2:
-                       table[pid1][pid2] += 1
+                        table[pid1][pid2] += 1
     return table
-    
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="this program generates a correlation-table from a CSV-file")
     parser.add_argument("input", help="The CSV input file (wikidata triple)")
