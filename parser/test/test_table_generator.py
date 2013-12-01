174f95793aa4dea04cdb4cb005898e7dbae5262e
Christian Dullweber <xchrdw@gmail.com>
2013-12-01 23:48:13 +0100

    more explicit asserts

--- parser/test/test_table_generator.py
+++ parser/test/test_table_generator.py
@@ -1,33 +1,48 @@
-from StringIO import StringIO
 import unittest
 from testtools import TestCase
 from testtools.matchers import *
-import gzip
 import TableGenerator
-import os
+
+test_data1 = [('Q15', [('31', 'wikibase-entityid', 'Q5107'),
+                       ('373', 'string', 'Africa')]),
+              ('Q16', [('31', 'wikibase-entityid', 'Q384')])]
+
+test_data2 = [('Q15', [('31', 'wikibase-entityid', 'Q5107'),
+                       ('373', 'string', 'Africa'),
+                       ('373', 'string', 'Europe')])]
+
 
 class TableGeneratorTest(TestCase):
-
     def setUp(self):
         TestCase.setUp(self)
 
-    def buildGen(self):
-        yield 'Q15', [('31', 'wikibase-entityid', 'Q5107'), ('373', 'string', 'Africa'), ('625', 'globecoordinate', 'N7.188056, E21.093611')]
-
     def testTableGenerator(self):
-        table = TableGenerator.computeTable(self.buildGen())
-        self.assertTrue('31' in table)
-        self.assertTrue('373' in table)
-        self.assertTrue('625' in table)
-        for pid1 in table:
-            self.assertThat(table[pid1]["appearances"], Equals(1))
-            self.assertThat(type(table[pid1]["type"]), Equals(str))
-            for pid2 in table[pid1]:
-                if pid2.isdigit():
-                    if pid1 == pid2:
-                        self.assertThat(table[pid1][pid2], Equals(0))
-                    else:
-                        self.assertThat(table[pid1][pid2], Equals(1))
+        table = TableGenerator.computeTable(test_data1)
+        self.assertThat(table, ContainsAll(('31', '373')))
+
+        self.assertThat(table['31']['appearances'], Equals(2))
+        self.assertThat(table['31']['type'], Equals('wikibase-entityid'))
+        self.assertThat(table['31']['31'], Equals(0))
+        self.assertThat(table['31']['373'], Equals(1))
+
+        self.assertThat(table['373']['appearances'], Equals(1))
+        self.assertThat(table['373']['type'], Equals('string'))
+        self.assertThat(table['373']['373'], Equals(0))
+        self.assertThat(table['373']['31'], Equals(1))
+
+    def testTableWithMultipleOccurance(self):
+        table = TableGenerator.computeTable(test_data2)
+
+        self.assertThat(table['31']['appearances'], Equals(1))
+        self.assertThat(table['31']['type'], Equals('wikibase-entityid'))
+        self.assertThat(table['31']['31'], Equals(0))
+        self.assertThat(table['31']['373'], Equals(2))
+
+        self.assertThat(table['373']['appearances'], Equals(2))
+        self.assertThat(table['373']['type'], Equals('string'))
+        self.assertThat(table['373']['373'], Equals(0))
+        self.assertThat(table['373']['31'], Equals(2))
+
 
 if __name__ == '__main__':
     unittest.main()
