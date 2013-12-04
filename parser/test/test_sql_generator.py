b1f44ba1b2cdb972a64c6fd94c6e5b6c4cc02b34
Christian Dullweber <xchrdw@gmail.com>
2013-12-04 19:49:18 +0100

    add sql generator test

---
+++ parser/test/test_sql_generator.py
@@ -0,0 +1,28 @@
+import unittest
+from testtools import TestCase
+# from testtools.matchers import *
+from mockito import mock, when, contains, verify, verifyNoMoreInteractions
+
+import SqlGenerator
+
+
+class SqlGeneratorTest(TestCase):
+    def setUp(self):
+        TestCase.setUp(self)
+        self.db = mock()
+
+    def testCreateTable(self):
+        table = {'1': {'appearances': 8, 'type': 'string', '1': 0, '2': 5}}
+        SqlGenerator.pushDictContentIntoDB(table, self.db)
+
+        verify(self.db).execute(contains("CREATE TABLE IF NOT EXISTS wbs_properties"))
+        verify(self.db).execute(contains("CREATE TABLE IF NOT EXISTS wbs_propertyPairs"))
+        verify(self.db).execute(contains("DELETE FROM wbs_properties"))
+        verify(self.db).execute(contains("DELETE FROM wbs_propertyPairs"))
+        verify(self.db).execute(contains("INSERT INTO wbs_properties"), ('1', 8.0, 'string'))
+        verify(self.db).execute(contains("INSERT INTO wbs_propertyPairs"), ('1', '2', 5, 5.0 / 8.0))
+        verifyNoMoreInteractions(self.db)
+
+
+if __name__ == '__main__':
+    unittest.main()
