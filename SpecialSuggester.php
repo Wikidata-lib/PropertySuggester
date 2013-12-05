73046e20af249da6c5433456990f45e0733cba66
Christian Dullweber <xchrdw@gmail.com>
2013-12-05 16:36:02 +0100

    remove old code

--- SpecialSuggester.php
+++ SpecialSuggester.php
@@ -33,6 +33,7 @@ class SpecialSuggester extends SpecialPage {
                 $out->addHTML("<input placeholder='Property' id='property-chooser' class='ui-autocomplete-input' autofocus>");
               
                 $out->addHTML("<input type='button' value='Add' id='add-property-btn'></input>");
+                
                 $out->addHTML("<p/>");
                 
                 $out->addHtml("<ul id='selected-properties-list'></ul>");                               
@@ -40,70 +41,7 @@ class SpecialSuggester extends SpecialPage {
                 $out->addHtml("<p/>");
                 		
                 $out->addHTML("<div id='result'></div>");
-                
-		$out->addHTML("Correlation values for all Attributes regarding item Q2 'Moritz Finke': <br/>");
-		
-		//Suggester testen:
-		$suggester = new SimplePHPSuggester();
-		$suggester->computeTable();
-		
-		$this->printTable($suggester->getPropertyRelations(), $out);
-		
-		$schnittstelle = StoreFactory::getStore( 'sqlstore' )->getEntityLookup();
-		$id = new EntityId( Item::ENTITY_TYPE, 2 );
-		$entity = $schnittstelle->getEntity($id);
-		
-		$results = $suggester->suggestionsByEntity($entity, 10);
-		foreach($results as $rank => $suggestion)
-		{
-			$out->addHTML($rank+1 . ". " . $suggestion->getPropertyId() . ":  " . $suggestion->getCorrelation() . "<br />");
-		}
-	}
+        }
 	
-	function incrementOrInit()
-	{
-	}
-	
-	function printTable($propertyRelations, $out)
-	{
-		$lookup = StoreFactory::getStore( 'sqlstore' )->getEntityLookup();
-		$entityPerPage = StoreFactory::getStore( 'sqlstore' )->newEntityPerPage();
-		
-		$properties = array();
-		$propertyIterator = $entityPerPage->getEntities(Property::ENTITY_TYPE);
-		foreach($propertyIterator as $key => $value)  {
-			$properties[] = $lookup->getEntity($value);
-		}
-		
-		$out->addHTML("<table border='1'><thead><tr>");
-		$out->addHTML("<th></th><th>Name</th><th>Appearances</th>");
-		foreach ($properties as $key => $value) {
-			$out->addHTML("<th>".$value->getLabel('de')."(".$value->getId()->getPrefixedId().")</th>");
-		}
-		$out->addHTML("</tr></thead><tbody>");
-		foreach ($properties as $key => $value) {
-			$propertyId1 = $value->getId()->getPrefixedId();
-			$out->addHTML("<tr><td>".$propertyId1."</td>");
-			$out->addHTML("<td>".$value->getLabel('de')."</td>");
-			if(isset($propertyRelations[$propertyId1])){
-				$out->addHTML("<td>".$propertyRelations[$propertyId1]['appearances']."</td>");
-			}else{
-				$out->addHTML("<td>0</td>");
-			}
-			foreach ($properties as $key => $value)
-			{
-				$propertyId2 = $value->getId()->getPrefixedId();
-				$out->addHTML("<td>");
-				if(isset($propertyRelations[$propertyId1][$propertyId2])){
-					$out->addHTML($propertyRelations[$propertyId1][$propertyId2]);
-				}else{
-					$out->addHTML("-");
-				}
-				$out->addHTML("</td>");
-			}
-			$out->addHTML("</tr>");
-		}
-		$out->addHTML("</tbody></table>");
-	}
 }
 
