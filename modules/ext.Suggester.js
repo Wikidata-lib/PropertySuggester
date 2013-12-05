4c6217fc4f15508391d8269c45c1be88bc783568
Christian Dullweber <xchrdw@gmail.com>
2013-12-05 15:19:31 +0100

    special page is working

--- modules/ext.Suggester.js
+++ modules/ext.Suggester.js
@@ -1,29 +1,35 @@
  var selected_ids = [];
-function deleteFromList(source){
-    alert(1);
-        
+ 
+ function removeFromArray(arr, element) {
+     arr.splice($.inArray(element, arr),1);
+ }
+ 
+function deleteFromList(evt){
+    pid = evt.data;
+    removeFromArray(selected_ids, pid);
+    $(this).closest("li").remove();
+    doQuery();
     return false;
 }
+
 function handleInput () {
     input_text =  $( "#property-chooser").val();
-    input_id = $("#property-chooser").next("input").val();
-    if (input_text!=  "" && input_id != ""){    
-        selected_ids.push(input_id);
-        delete_link = " <a href='#' onclick=\"deleteFromList(this)\"> x </a> ";
-        li_element = "<li>" + input_text + " (" + input_id + ")" + delete_link + "</input></li>";
+    pid = $("#property-chooser").next("input").val();
+    if (input_text!=  "" && pid != ""){    
+        selected_ids.push(pid);
+        delete_link = $("<a href='#'>x</a>").click(pid, deleteFromList);
+        li_element = $("<li>" + input_text + " (" + pid + ")" + "</input></li>");
+        li_element.append(delete_link);
         $("#selected-properties-list").append(li_element);
-        $( "#property-chooser").val('');
-        doQuery()
+        $( "#property-chooser").val('').focus();
+        doQuery();
     }
-    //\\\"" + input_id + "\\\"
 }
-//onclick='deleteFromList(this)
-
 
 function doQuery() {
-    url = "api.php?properties=" + encodeURIComponent(selected_ids.join(","));
+    url = "api.php?properties=" + selected_ids.map(encodeURIComponent).join(",");
     $.get("http://localhost/devrepo/api.php?action=wbsearchentities&format=json&language=de&type=property&search=v", function( data ) {
-        $("#result").text(JSON.stringify(data));
+        $("#result").text(url + "   " +JSON.stringify(data));
     });
 }
 
@@ -36,17 +42,13 @@ $( document ).ready(function (){
     
     $( "#add-property-btn" ).click(function() {
         handleInput();
-      
     });
     
 
     $("input.ui-autocomplete-input").keyup(function (e) {
-    if (e.keyCode == 13) {
-        handleInput();
-    
-    }
-});
-    
-    // delete from list
+        if (e.keyCode === 13) {
+            handleInput();
+        }
+    });
     
 });
