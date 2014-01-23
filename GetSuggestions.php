8f74053d255f9cc3ab4b1ad8034ec76ebf502894
Felix Niemeyer <felix.niemeyer@student.hpi.uni-potsdam.de>
2014-01-23 15:29:22 +0100

    adapting GetSuggestions to coding style conventions

--- GetSuggestions.php
+++ GetSuggestions.php
@@ -19,83 +19,83 @@ include 'Suggesters/SimplePHPSuggester.php';
  */
 
 function cleanPropertyId( $propertyId ) {
-    if ( $propertyId[0] === 'P' ) {
-            return (int)substr( $propertyId, 1 );
-    }
-    return (int)$propertyId;
+	if ( $propertyId[0] === 'P' ) {
+			return (int)substr( $propertyId, 1 );
+	}
+	return (int)$propertyId;
 }
 
 class GetSuggestions extends ApiBase {
 
-    public function __construct( ApiMain $main, $name, $search = '' ) {
-            parent::__construct( $main, $name, $search );
-    }
+	public function __construct( ApiMain $main, $name, $search = '' ) {
+			parent::__construct( $main, $name, $search );
+	}
 
-    /**
-     * @see ApiBase::execute()
-     */
-    public function execute() {
-        $params = $this->extractRequestParams();
-		
+	/**
+	 * @see ApiBase::execute()
+	 */
+	public function execute() {
+		$params = $this->extractRequestParams();
+
 		// Understand params
-        if ( ! ( isset( $params['entity'] ) xor isset( $params['properties'] ) ) ) {
-                wfProfileOut( __METHOD__ );
-                $this->dieUsage( 'provide either entity id parameter \'entity\' or list of properties \'properties\'', 'param-missing' );
-        }
+		if ( ! ( isset( $params['entity'] ) xor isset( $params['properties'] ) ) ) {
+				wfProfileOut( __METHOD__ );
+				$this->dieUsage( 'provide either entity id parameter \'entity\' or list of properties \'properties\'', 'param-missing' );
+		}
 
-        if ( isset( $params['search'] ) && $params['search'] != '*' ) {
-            $search = $params['search'];
-        } else {
-            $search = '';
-        }
+		if ( isset( $params['search'] ) && $params['search'] != '*' ) {
+			$search = $params['search'];
+		} else {
+			$search = '';
+		}
 		$language = 'en';
 		if ( isset( $params['language'] ) ) { // TODO: use fallback
 				$language = $params['language'];
 		}
-        $limit = $params['limit'];
-        $continue = $params['continue'];
+		$limit = $params['limit'];
+		$continue = $params['continue'];
 		$resultSize = $continue + $limit;
 
-		$entries = $this->generateSuggestions($params["entity"], $params['properties'][0], $search, $language);
-		
+		$entries = $this->generateSuggestions( $params["entity"], $params['properties'][0], $search, $language );
+
 		if ( count( $entries ) < $resultSize && $search ) {
 			$entries = $this->mergeWithTraditionalSearchResults( $entries, $resultSize, $search, $language );
 		}
 
 		// Define Result
 		$slicedEntries = array_slice( $entries, $continue, $limit );
-        $this->getResult()->addValue( null, 'search', $slicedEntries );
-        $this->getResult()->addValue( null, 'success', 1 );
-        if ( count( $entries ) > $resultSize ) {
-            $this->getResult()->addValue( null, 'search-continue', $resultSize );
-        }
-        $this->getResult()->addValue( 'searchinfo', 'search', $search );
-    }
-	
-	public function generateSuggestions($entity, $propertyList, $search, $language)
+		$this->getResult()->addValue( null, 'search', $slicedEntries );
+		$this->getResult()->addValue( null, 'success', 1 );
+		if ( count( $entries ) > $resultSize ) {
+			$this->getResult()->addValue( null, 'search-continue', $resultSize );
+		}
+		$this->getResult()->addValue( 'searchinfo', 'search', $search );
+	}
+
+	public function generateSuggestions( $entity, $propertyList, $search, $language )
 	{
 		$suggester = new SimplePHPSuggester();
-        $lookup = StoreFactory::getStore( 'sqlstore' )->getEntityLookup();
-        if ( isset( $entity ) ) {
-                $id = new  ItemId( $entity );
-                $entity = $lookup->getEntity( $id );
-                $suggestions = $suggester->suggestionsByItem( $entity, 1000 );
-        } else {
-                $splittedList = explode( ',', $propertyList );
-                $intList = array_map( 'cleanPropertyId', $splittedList );
+		$lookup = StoreFactory::getStore( 'sqlstore' )->getEntityLookup();
+		if ( isset( $entity ) ) {
+				$id = new  ItemId( $entity );
+				$entity = $lookup->getEntity( $id );
+				$suggestions = $suggester->suggestionsByItem( $entity, 1000 );
+		} else {
+				$splittedList = explode( ',', $propertyList );
+				$intList = array_map( 'cleanPropertyId', $splittedList );
 
-                $suggestions = $suggester->suggestionsByAttributeList( $intList, 1000 );
-        }
+				$suggestions = $suggester->suggestionsByAttributeList( $intList, 1000 );
+		}
 
 		// Build result Array
-        $entries = $this->createJSON( $suggestions, $language, $lookup );
+		$entries = $this->createJSON( $suggestions, $language, $lookup );
 		if ( $search )	{
 			$entries = $this->filterByPrefix( $entries, $search );
 		}
 		return $entries;
 	}
-	
-	public function mergeWithTraditionalSearchResults(& $entries, $resultSize, $search, $language)
+
+	public function mergeWithTraditionalSearchResults( & $entries, $resultSize, $search, $language )
 	{
 		$searchEntitiesParameters = new DerivativeRequest(
 			$this->getRequest(),
@@ -131,7 +131,7 @@ class GetSuggestions extends ApiBase {
 		}
 		return array_merge( $entries, $noDuplicateEntries );
 	}
-	
+
 	public function filterByPrefix( $entries, $search )
 	{
 		$matchingEntries = array();
@@ -145,39 +145,39 @@ class GetSuggestions extends ApiBase {
 
 	public function createJSON( $suggestions, $language, $lookup ) {
 		$entries = array();
-        foreach ( $suggestions as $suggestion ) {
-            $entry = array();
-            $id = new PropertyId( 'P' . $suggestion->getPropertyId() );
-            $property = $lookup->getEntity( $id );
+		foreach ( $suggestions as $suggestion ) {
+			$entry = array();
+			$id = new PropertyId( 'P' . $suggestion->getPropertyId() );
+			$property = $lookup->getEntity( $id );
 			$entityContentFactory = WikibaseRepo::getDefaultInstance()->getEntityContentFactory();
 
-            if ( $property == null ) {
-                continue;
-            }
-            $entry['id'] = 'P' . $suggestion->getPropertyId();
-            $entry['label'] = $property->getLabel( $language );
+			if ( $property == null ) {
+				continue;
+			}
+			$entry['id'] = 'P' . $suggestion->getPropertyId();
+			$entry['label'] = $property->getLabel( $language );
 			$entry['description'] = $property->getDescription( $language );
 			$entry['correlation'] = $suggestion->getCorrelation();
 			$entry['url'] = $entityContentFactory->getTitleForId( $id )->getFullUrl();
 			$entry['debug:type'] = 'suggestion'; // debug
-            $entries[] = $entry;
-        }
+			$entries[] = $entry;
+		}
 		return $entries;
 	}
 
-    /**
-     * @see ApiBase::getAllowedParams()
-     */
-    public function getAllowedParams() {
-        return array(
-            'entity' => array(
+	/**
+	 * @see ApiBase::getAllowedParams()
+	 */
+	public function getAllowedParams() {
+		return array(
+			'entity' => array(
 				ApiBase::PARAM_TYPE => 'string',
-            ),
-            'properties' => array(
+			),
+			'properties' => array(
 				ApiBase::PARAM_TYPE => 'string',
 				ApiBase::PARAM_ISMULTI => true,
 				ApiBase::PARAM_ALLOW_DUPLICATES => false
-            ),
+			),
 			'limit' => array(
 				ApiBase::PARAM_TYPE => 'limit',
 				ApiBase::PARAM_DFLT => 7,
@@ -194,51 +194,51 @@ class GetSuggestions extends ApiBase {
 				ApiBase::PARAM_MIN => 0,
 				ApiBase::PARAM_RANGE_ENFORCE => true,
 			),
-            'language' => array(
+			'language' => array(
 				ApiBase::PARAM_TYPE => Utils::getLanguageCodes(),
-            ),
+			),
 			'search' => array(
 				ApiBase::PARAM_TYPE => 'string',
 			)
 		);
-    }
+	}
 
-    /**
-     * @see ApiBase::getParamDescription()
-     */
-    public function getParamDescription() {
-            return array_merge( parent::getParamDescription(), array(
-                    'entity' => 'Suggest attributes for given entity',
-                    'properties' => 'Identifier for the site on which the corresponding page resides',
-                    'size' => 'Specify number of suggestions to be returned',
-                    'language' => 'language for result',
+	/**
+	 * @see ApiBase::getParamDescription()
+	 */
+	public function getParamDescription() {
+			return array_merge( parent::getParamDescription(), array(
+					'entity' => 'Suggest attributes for given entity',
+					'properties' => 'Identifier for the site on which the corresponding page resides',
+					'size' => 'Specify number of suggestions to be returned',
+					'language' => 'language for result',
 					'limit' => 'Maximal number of results',
 					'continue' => 'Offset where to continue a search'
-            ) );
-    }
+			) );
+	}
 
-    /**
-     * @see ApiBase::getDescription()
-     */
-    public function getDescription() {
-        return array(
-                'API module to get property suggestions.'
-        );
-    }
+	/**
+	 * @see ApiBase::getDescription()
+	 */
+	public function getDescription() {
+		return array(
+				'API module to get property suggestions.'
+		);
+	}
 
-    /**
-     * @see ApiBase::getPossibleErrors()
-     */
-    public function getPossibleErrors() {
-        return array_merge( parent::getPossibleErrors(), array(
-                array( 'code' => 'param-missing', 'info' => $this->msg( 'wikibase-api-param-missing' )->text() )
-        ) );
-    }
+	/**
+	 * @see ApiBase::getPossibleErrors()
+	 */
+	public function getPossibleErrors() {
+		return array_merge( parent::getPossibleErrors(), array(
+				array( 'code' => 'param-missing', 'info' => $this->msg( 'wikibase-api-param-missing' )->text() )
+		) );
+	}
 
-    /**
-     * @see ApiBase::getExamples()
-     */
-    protected function getExamples() {
-        return array();
-    }
+	/**
+	 * @see ApiBase::getExamples()
+	 */
+	protected function getExamples() {
+		return array();
+	}
 }
