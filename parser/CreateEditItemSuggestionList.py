a5106a6764ad76abb142b672b55ef646ea94710a
Dacry <moritz.finke@student.hpi.uni-potsdam.de>
2014-02-23 15:09:27 +0100

    update code to fit the new data model

--- parser/CreateEditItemSuggestionList.py
+++ parser/CreateEditItemSuggestionList.py
@@ -1,12 +1,14 @@
 import CsvReader
 import random
 import argparse
+from WikidataApi import WikidataApi
 from CompressedFileType import CompressedFileType
 
 import json
 import urllib2
 
 pathToWiki = 'http://suggester.wmflabs.org/wiki'
+threshold = 0.3 # threshold that suggestions in the results have to pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="this program generates a Edit-Item-Suggestions")
@@ -19,7 +21,7 @@ if __name__ == "__main__":
     itemCount=0
     pidList = args.properties.split(",")
     
-    for title, claims in CsvReader.read_csv(args.input):
+    for entity in CsvReader.read_csv(args.input):
         itemCount +=1
         if itemCount%100 == 0:
             print str(itemCount)
@@ -27,18 +29,17 @@ if __name__ == "__main__":
             continue
         if itemCount > args.limit:
             break
-        if claims:
-            propertyIds = [i[0] for i in claims] #get ids from claims
-            propertieParameters = "%2C%20".join(propertyIds[:-1])
-            url = pathToWiki + '/api.php?action=wbsgetsuggestions&format=json&properties='+propertieParameters+'&limit=50&continue=0&language=en'
-            suggestions = json.load(urllib2.urlopen(url))["search"]
+        if entity.claims:
+            propertyIds = [claim.property_id for claim in entity.claims] #get ids from claims
+            api = WikidataApi(pathToWiki)
+            result = api.wbs_getsuggestions(properties=propertyIds, limit=50, cont=0)
+            suggestions = result["search"]
             suggestIds = ""
             for suggestion in suggestions:
-                if float(suggestion["correlation"]) > 0.3:
+                if float(suggestion["correlation"]) > threshold:
                     if args.properties == "" or (str(suggestion["id"][1:]) in pidList):
                         suggestIds = suggestIds + " " + str(suggestion["id"]) 
                 else:
                     if suggestIds != "":
-                        print 'http://www.wikidata.org/wiki'+'/Item:' + title + suggestIds
+                        print 'http://www.wikidata.org/wiki'+'/Item:' + entity.title + suggestIds
                     break;
-
