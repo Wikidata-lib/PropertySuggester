b3aa74f52164330bcf9f50dc54acacc230a87879
Christian Dullweber <xchrdw@gmail.com>
2014-02-26 16:15:29 +0100

    fix use

--- tests/phpunit/GetSuggestionsTest.php
+++ tests/phpunit/GetSuggestionsTest.php
@@ -1,37 +1,38 @@
 <?php
 
-namespace Wikibase\Test\Api;
+namespace PropertySuggester\Test\Api;
+
+use Wikibase\Test\Api\WikibaseApiTestCase;
+
 /**
  *
  * @covers PropertySuggester\GetSuggestions
- * 
+ *
  * @group Extensions/PropertySuggester
- * 
+ *
  * @group API
  * @group Database
  *
  * @group medium
- * 
+ *
  */
-class GetSuggestionsTest extends WikibaseApiTestCase { 
-	
-	public function setUp()
-	{
+class GetSuggestionsTest extends WikibaseApiTestCase {
+
+	public function setUp() {
 		parent::setUp();
 		//own setup
 	}
-	
+
 	/*
 	 * @group Extensions/GetSuggestions
 	 */
 	public function testFilterByPrefix() {
-		$this->assertEquals(0,0, "XXXXX");
-		$this->assertEquals(1,1, "XXXXX");
-		$this->assertEquals(2,2, "XXXXX");
+		$this->assertEquals( 0, 0, "XXXXX" );
+		$this->assertEquals( 1, 1, "XXXXX" );
+		$this->assertEquals( 2, 2, "XXXXX" );
 	}
-	
-	public function tearDown()
-	{
+
+	public function tearDown() {
 		//own tear downs
 		parent::tearDown();
 	}
