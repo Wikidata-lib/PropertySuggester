fc52c25beeead013bf2dbe85576fd86b561aaadb
Dacry <moritz.finke@student.hpi.uni-potsdam.de>
2014-03-05 03:01:51 +0100

    testDeprecated Properties...

--- tests/phpunit/PropertySuggester/Suggesters/SimplePHPSuggesterTest.php
+++ tests/phpunit/PropertySuggester/Suggesters/SimplePHPSuggesterTest.php
@@ -35,33 +35,33 @@ class SimplePHPSuggesterTest extends MediaWikiTestCase {
 	protected $suggester;
 
 
-	private function row($pid1, $pid2, $count, $probability) {
-		return array('pid1' => $pid1, 'pid2' => $pid2, 'count' => $count, 'probability' => $probability);
+	private function row( $pid1, $pid2, $count, $probability ) {
+		return array( 'pid1' => $pid1, 'pid2' => $pid2, 'count' => $count, 'probability' => $probability );
 	}
 
 	public function addDBData() {
 		$rows = array();
-		$rows[] = $this->row(1, 2, 100, 0.1);
-		$rows[] = $this->row(1, 3, 50, 0.05);
-		$rows[] = $this->row(2, 3, 100, 0.1);
-		$rows[] = $this->row(2, 4, 200, 0.2);
-		$rows[] = $this->row(3, 1, 100, 0.5);
+		$rows[] = $this->row( 1, 2, 100, 0.1 );
+		$rows[] = $this->row( 1, 3, 50, 0.05 );
+		$rows[] = $this->row( 2, 3, 100, 0.1 );
+		$rows[] = $this->row( 2, 4, 200, 0.2 );
+		$rows[] = $this->row( 3, 1, 100, 0.5 );
 
 		$dbw = wfGetDB( DB_MASTER );
-		$dbw->delete('wbs_propertypairs', "*");
-		$dbw->insert('wbs_propertypairs', $rows);
+		$dbw->delete( 'wbs_propertypairs', "*" );
+		$dbw->insert( 'wbs_propertypairs', $rows );
 	}
 
 	public function setUp() {
 		parent::setUp();
 		$this->dbr = wfGetDB( DB_SLAVE );
-		$this->suggester = new SimplePHPSuggester($this->dbr);
+		$this->suggester = new SimplePHPSuggester( $this->dbr );
 
 	}
 
 	public function testDatabaseHasRows() {
-		$res = $this->dbr->select('wbs_propertypairs', array( 'pid1', 'pid2'));
-		$this->assertEquals(5, $res->numRows());
+		$res = $this->dbr->select( 'wbs_propertypairs', array( 'pid1', 'pid2') );
+		$this->assertEquals( 5, $res->numRows() );
 	}
 
 	public function testSuggestByPropertyIds() {
@@ -69,8 +69,8 @@ class SimplePHPSuggesterTest extends MediaWikiTestCase {
 
 		$res = $this->suggester->suggestByPropertyIds($ids);
 
-		$this->assertEquals(PropertyId::newFromNumber(2), $res[0]->getPropertyId());
-		$this->assertEquals(PropertyId::newFromNumber(3), $res[1]->getPropertyId());
+		$this->assertEquals( PropertyId::newFromNumber( 2 ), $res[0]->getPropertyId() );
+		$this->assertEquals( PropertyId::newFromNumber( 3 ), $res[1]->getPropertyId() );
 	}
 
 	public function testSuggestByItem() {
@@ -81,8 +81,19 @@ class SimplePHPSuggesterTest extends MediaWikiTestCase {
 
 		$res = $this->suggester->suggestByItem($item);
 
-		$this->assertEquals(PropertyId::newFromNumber(2), $res[0]->getPropertyId());
-		$this->assertEquals(PropertyId::newFromNumber(3), $res[1]->getPropertyId());
+		$this->assertEquals( PropertyId::newFromNumber( 2 ), $res[0]->getPropertyId() );
+		$this->assertEquals( PropertyId::newFromNumber( 3 ), $res[1]->getPropertyId() );
+	}
+
+	public function testDeprecatedProperties() {
+		$ids = array( PropertyId::newFromNumber( 1 ) );
+
+		$this->suggester->setDeprecatedPropertyIds( array( 2 ) );
+
+		$res = $this->suggester->suggestByPropertyIds( $ids );
+
+		$resIds = array_map( function( $r ) { return $r->getPropertyId()->getSerialization(); }, $res );
+		$this->assertNotContains( "P2", $resIds );
 	}
 
 	public function tearDown() {
