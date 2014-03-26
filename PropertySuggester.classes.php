c4ca8fcbe8676c91f506cac7e4e7fcf35829bf86
Christian Dullweber <xchrdw@gmail.com>
2014-03-26 14:55:21 +0100

    add composer

---
+++ PropertySuggester.classes.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class registration file for PropertySuggester.
+ */
+return array(
+	'PropertySuggesterHooks' => 'PropertySuggesterHooks.php',
+
+	'PropertySuggester\SpecialSuggester' => 'src/PropertySuggester/SpecialSuggester.php',
+
+	'PropertySuggester\GetSuggestions' => 'src/PropertySuggester/GetSuggestions.php',
+	'PropertySuggester\GetSuggestionsHelper' => 'src/PropertySuggester/GetSuggestionsHelper.php',
+	
+	'PropertySuggester\Suggesters\Suggestion' => 'src/PropertySuggester/Suggesters/Suggestion.php',
+	'PropertySuggester\Suggesters\SuggesterEngine' => 'src/PropertySuggester/Suggesters/SuggesterEngine.php',
+	'PropertySuggester\Suggesters\SimplePHPSuggester' => 'src/PropertySuggester/Suggesters/SimplePHPSuggester.php',
+);
