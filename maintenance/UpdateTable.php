dffb0a33b90dbd852120f83afe092196468fc345
Christian Dullweber <xchrdw@gmail.com>
2014-03-26 13:03:34 +0100

    fix some formatting

--- maintenance/UpdateTable.php
+++ maintenance/UpdateTable.php
@@ -19,7 +19,7 @@ class UpdateTable extends Maintenance {
 		$this->mDescription = "Read CSV Dump and refill probability table";
 		$this->addOption( 'file', 'CSV table to be loaded (relative path)', true, true );
 		$this->addOption( 'use-insert', 'Avoid DBS specific import. Use INSERTs.', false, false );
-		$this->addOption( 'silent', 'Don not show information', false, false );
+		$this->addOption( 'silent', 'Do not show information', false, false );
 	}
 
 
@@ -28,7 +28,7 @@ class UpdateTable extends Maintenance {
 		$csv = null;
 		if ( substr( $this->getOption( 'file' ), 0, 2 ) === "--" ) {
 			$this->error( "The --file option requires a file as an argument.\n", true );
-		} elseif ($this->hasOption( 'file' )) {
+		} elseif ( $this->hasOption( 'file' ) ) {
 			$csv = $this->getOption( 'file' );
 		}
 		$useInsert = $this->getOption( 'use-insert' );
@@ -42,63 +42,56 @@ class UpdateTable extends Maintenance {
 
 		global $wgDBtype;
 		$tablename = 'wbs_propertypairs';
-		$dbtablename = $db->tableName($tablename);
-		if( $db->tableExists($tablename))
-		{
-			if($showInfo)$this->output( "removing old entries\n" );
-			$db->delete( $tablename, '*');
-			if($showInfo)$this->output( "... Done removing\n" );
-		} else
-		{
-			$this->error( "$dbtablename table does not exist.\nExecuting core/maintenance/update.php may help.\n", true);
+		$dbtablename = $db->tableName( $tablename );
+		if ( $db->tableExists( $tablename ) ) {
+			if ( $showInfo ) $this->output( "removing old entries\n" );
+			$db->delete( $tablename, '*' );
+			if ( $showInfo ) $this->output( "... Done removing\n" );
+		} else {
+			$this->error( "$dbtablename table does not exist.\nExecuting core/maintenance/update.php may help.\n", true );
 		}
 
-		if($showInfo)$this->output( "loading new entries from file\n" );
-		$wholePath = str_replace( '\\', '/', __DIR__ . "/" . $csv);
+		if ( $showInfo ) $this->output( "loading new entries from file\n" );
+		$wholePath = str_replace( '\\', '/', __DIR__ . "/" . $csv );
 
-		if($wgDBtype == 'mysql' and !$useInsert)
-		{
-			$db->query("
+		if ( $wgDBtype == 'mysql' and !$useInsert ) {
+			$db->query( "
 				LOAD DATA INFILE '$wholePath'
 				INTO TABLE $dbtablename
 				FIELDS
 					TERMINATED BY ';'
 				LINES
 					TERMINATED BY '\\n'
-			");
-		}elseif($wgDBtype == 'postgres' and !$useInsert) //not tested yet
-		{
-			$db->query("
+			" );
+		} elseif ( $wgDBtype == 'postgres' and !$useInsert ) {
+			$db->query( "
 				COPY $dbtablename
 				FROM '$wholePath'
 				WITH
 					DELIMITER ';'
-			");
-		}else{
-			$fhandle = fopen($wholePath, "r");
-			if($fhandle !== false)
-			{
+			" );
+		} else {
+			$fhandle = fopen( $wholePath, "r" );
+			if ( $fhandle !== false ) {
 				$accuSize = 0;
 				$accu = Array();
-				$data = fgetcsv($fhandle,0,';');
-				while($data !== false)
-				{
+				$data = fgetcsv( $fhandle, 0, ';' );
+				while ( $data !== false ) {
 					$accu[] = array( 'pid1' => $data[0], 'pid2' => $data[1], 'count' => $data[2], 'probability' => $data[3] );
 					$accuSize++;
 
-					$data = fgetcsv($fhandle,0,';');
+					$data = fgetcsv( $fhandle, 0, ';' );
 
-					if($data === false or $accuSize > 1000)
-					{
-						$db->insert($tablename, $accu);
+					if ( $data === false or $accuSize > 1000 ) {
+						$db->insert( $tablename, $accu );
 						$accu = Array();
 						$accuSize = 0;
 					}
 				}
 			}
-			fclose($fhandle);
+			fclose( $fhandle );
 		}
-		if($showInfo)$this->output( "... Done loading\n" );
+		if ( $showInfo ) $this->output( "... Done loading\n" );
 	}
 }
 
