c0360e79490add5ec63765f250312a378ecbaf82
Christian Dullweber <xchrdw@gmail.com>
2014-04-26 00:08:06 +0200

    split tests

--- tests/phpunit/PropertySuggester/SuggesterParamsParserTest.php
+++ tests/phpunit/PropertySuggester/SuggesterParamsParserTest.php
@@ -26,7 +26,9 @@ class SuggesterParamsParserTest extends MediaWikiTestCase {
 
 	protected $defaultSuggesterResultSize = 100;
 	protected $defaultMinProbability = 0.01;
-
+    protected $defaultParams = array( 'entity' => null, 'properties' => null, 'continue' => 10, 'limit' => 5,
+									  'language' => 'en', 'search' => '' );
+    
 	public function setUp() {
 		parent::setUp();
 		$this->api = $this->getMockBuilder( 'PropertySuggester\GetSuggestions' )->disableOriginalConstructor()->getMock();
@@ -34,7 +36,9 @@ class SuggesterParamsParserTest extends MediaWikiTestCase {
 	}
 
 	public function testSuggesterParameters() {
-		$params = $this->paramsParser->parseAndValidate( array( 'entity' => 'Q1', 'properties' => null, 'continue' => 10, 'limit' => 5, 'language' => 'en', 'search' => '*') );
+		$params = $this->paramsParser->parseAndValidate(
+			array_merge( $this->defaultParams, array( 'entity' => 'Q1', 'search' => '*') )
+		);
 
 		$this->assertEquals( 'Q1', $params->entity );
 		$this->assertEquals( null, $params->properties );
@@ -47,7 +51,9 @@ class SuggesterParamsParserTest extends MediaWikiTestCase {
 	}
 
 	public function testSuggesterWithSearchParameters() {
-		$params = $this->paramsParser->parseAndValidate( array( 'entity' => null, 'properties' => array('P31'), 'continue' => 10, 'limit' => 5, 'language' => 'en', 'search' => 'asd') );
+		$params = $this->paramsParser->parseAndValidate(
+			array_merge( $this->defaultParams, array( 'properties' => array('P31'), 'search' => 'asd') )
+		);
 
 		$this->assertEquals( null, $params->entity );
 		$this->assertEquals( array( 'P31' ), $params->properties );
@@ -60,10 +66,19 @@ class SuggesterParamsParserTest extends MediaWikiTestCase {
 	}
 
 	public function testSuggesterWithInvalidParameters(){
-		$this->api->expects( $this->exactly(2) )
-			->method( 'dieUsage' );
-		$this->paramsParser->parseAndValidate( array( 'entity' => 'Q1', 'properties' => array('P31'), 'continue' => 10, 'limit' => 5, 'language' => 'en', 'search' => 'asd') );
-		$this->paramsParser->parseAndValidate( array( 'entity' => null, 'properties' => null, 'continue' => 10, 'limit' => 5, 'language' => 'en', 'search' => 'asd') );
+		$this->api->expects( $this->exactly(1) )->method( 'dieUsage' );
+
+		$this->paramsParser->parseAndValidate(
+			array_merge( $this->defaultParams, array( 'entity' => 'Q1', 'properties' => array('P31') ) )
+		);
+	}
+
+	public function testSuggesterWithMissingParameters(){
+		$this->api->expects( $this->exactly(1) )->method( 'dieUsage' );
+
+		$this->paramsParser->parseAndValidate(
+			array_merge( $this->defaultParams, array( 'entity' => null, 'properties' => null ) )
+		);
 	}
 
 }
