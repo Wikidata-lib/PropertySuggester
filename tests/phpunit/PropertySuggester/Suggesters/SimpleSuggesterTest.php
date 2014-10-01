90c49783dfd48ba6d34b43d9726a43e7d13f11a9
Dacry <moritz.finke@student.hpi.uni-potsdam.de>
2014-10-01 16:59:42 +0200

    fix tests

--- tests/phpunit/PropertySuggester/Suggesters/SimpleSuggesterTest.php
+++ tests/phpunit/PropertySuggester/Suggesters/SimpleSuggesterTest.php
@@ -5,7 +5,7 @@ namespace PropertySuggester\Suggesters;
 use LoadBalancerSingle;
 use InvalidArgumentException;
 use MediaWikiTestCase;
-use Wikibase\DataModel\Claim\Statement;
+use Wikibase\DataModel\Statement\Statement;
 use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\DataModel\Entity\PropertyId;
@@ -33,11 +33,11 @@ class SimpleSuggesterTest extends MediaWikiTestCase {
 
 	public function addDBData() {
 		$rows = array();
-		$rows[] = $this->row( 1, null, 2, 100, 0.1, 'item' );
-		$rows[] = $this->row( 1, null, 3, 50, 0.05, 'item' );
-		$rows[] = $this->row( 2, null, 3, 100, 0.3, 'item' );
-		$rows[] = $this->row( 2, null, 4, 200, 0.2, 'item' );
-		$rows[] = $this->row( 3, null, 1, 100, 0.5, 'item' );
+		$rows[] = $this->row( 1, 0, 2, 100, 0.1, 'item' );
+		$rows[] = $this->row( 1, 0, 3, 50, 0.05, 'item' );
+		$rows[] = $this->row( 2, 0, 3, 100, 0.3, 'item' );
+		$rows[] = $this->row( 2, 0, 4, 200, 0.2, 'item' );
+		$rows[] = $this->row( 3, 0, 1, 100, 0.5, 'item' );
 
 		$this->db->insert( 'wbs_propertypairs', $rows );
 	}
@@ -71,7 +71,7 @@ class SimpleSuggesterTest extends MediaWikiTestCase {
 		$item->setId( new ItemId( 'Q42' ) );
 		$statement = new Statement( new PropertySomeValueSnak( new PropertyId( 'P1' ) ) );
 		$statement->setGuid( 'claim0' );
-		$item->addClaim( $statement );
+		$item->getStatements()->addStatement( $statement );
 
 		$res = $this->suggester->suggestByItem( $item, 100, 0.0, 'item' );
 
