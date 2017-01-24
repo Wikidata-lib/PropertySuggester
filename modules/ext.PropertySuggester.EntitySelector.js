eb6accfc106467c0e1dbbc83ae9e5e97ec5224d2
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2017-01-24 11:18:20 +0100

    Add JSCS rule set and fix all code style warnings (#185)

--- modules/ext.PropertySuggester.EntitySelector.js
+++ modules/ext.PropertySuggester.EntitySelector.js
@@ -5,7 +5,7 @@
  * @see ui.suggester._request
  */
 
-( function( $ ) {
+( function ( $ ) {
 	'use strict';
 
 	var Item = $.wikibase.entityselector.Item;
@@ -26,26 +26,24 @@
 		 * @inheritdoc
 		 * @protected
 		 */
-		_create: function() {
+		_create: function () {
 			var self = this;
 
 			this._oldCreate.apply( this, arguments );
 
-			var focusHandler = function( event ) {
-				if ( self._useSuggester() &&
-					self.element.val() === '' &&
-					!self.options.menu.element.is( ':visible' )
+			// Search for suggestions once the field is initially focused.
+			// We only need to do this once, afterwards the old suggestions
+			// will re-appear on focus anyway.
+			this.element.one( 'focus', function ( event ) {
+				if ( self._useSuggester()
+					&& self.element.val() === ''
+					&& !self.options.menu.element.is( ':visible' )
 				) {
 					self.options.minTermLength = 0;
 					self._cache = {}; // is done in the entityselector on eachchange too
 					self.search();
 				}
-			};
-
-			// Search for suggestions once the field is initially focused.
-			// We only need to do this once, afterwards the old suggestions
-			// will re-appear on focus anyway.
-			this.element.one( 'focus', focusHandler );
+			} );
 		},
 
 		/**
@@ -60,34 +58,38 @@
 		 * @param {string} term
 		 * @return {Object}
 		 */
-		_getSearchApiParameters: function( term ) {
+		_getSearchApiParameters: function ( term ) {
+			var data;
+
 			if ( !this._useSuggester() ) {
 				return this._oldGetSearchApiParameters( term );
-			} else {
-				var data = {
-					action: 'wbsgetsuggestions',
-					search: term,
-					context: this._getPropertyContext(),
-					format: 'json',
-					language: this.options.language,
-					'continue': this._cache.term === term && this._cache.nextSuggestionOffset ? this._cache.nextSuggestionOffset : 0
-				};
-
-				if ( data.context === 'item' ) {
-					data.entity = this._getEntity().getId();
-				} else {
-					data.properties = this._getPropertyId();
-				}
-
-				return data;
 			}
+
+			data = {
+				action: 'wbsgetsuggestions',
+				search: term,
+				context: this._getPropertyContext(),
+				format: 'json',
+				language: this.options.language,
+				'continue': this._cache.term === term && this._cache.nextSuggestionOffset
+					? this._cache.nextSuggestionOffset
+					: 0
+			};
+
+			if ( data.context === 'item' ) {
+				data.entity = this._getEntity().getId();
+			} else {
+				data.properties = this._getPropertyId();
+			}
+
+			return data;
 		},
 
 		/**
 		 * @private
 		 * @return {boolean}
 		 */
-		_useSuggester: function() {
+		_useSuggester: function () {
 			var entity = this._getEntity();
 
 			return this.options.type === 'property' && entity && entity.getType() === 'item';
@@ -95,47 +97,57 @@
 
 		/**
 		 * Get the entity from the surrounding entityview or return null
+		 *
 		 * @private
 		 * @return {wikibase.Entity|null}
 		 */
-		_getEntity: function() {
+		_getEntity: function () {
 			var $entityView;
+
 			try {
 				$entityView = this.element.closest( ':wikibase-entityview' );
 			} catch ( ex ) {
 				return null;
 			}
 
-			return $entityView.length > 0 ? $entityView.data( 'entityview' ).option( 'value' ) : null;
+			return $entityView.length > 0
+				? $entityView.data( 'entityview' ).option( 'value' )
+				: null;
 		},
 
 		/**
-		 * Returns the property id for the enclosing statementview or null if no property is selected yet.
+		 * Returns the property id for the enclosing statementview or null if no property is
+		 * selected yet.
 		 *
 		 * @private
 		 * @return {string|null}
 		 */
-		_getPropertyId: function() {
-			var $statementview;
+		_getPropertyId: function () {
+			var $statementview,
+				statement;
+
 			try {
 				$statementview = this.element.closest( ':wikibase-statementview' );
 			} catch ( ex ) {
 				return null;
 			}
 
-			var statement = $statementview.length > 0 ? $statementview.data( 'statementview' ).option( 'value' ) : null;
+			statement = $statementview.length > 0
+				? $statementview.data( 'statementview' ).option( 'value' )
+				: null;
 
 			return statement ? statement.getClaim().getMainSnak().getPropertyId() : null;
 		},
 
 		/**
-		 * Returns either 'item', 'qualifier', 'reference' or null depending on the context of the entityselector.
-		 * 'item' is returned in case that the selector is for a new property in an item.
+		 * Returns either 'item', 'qualifier', 'reference' or null depending on the context of the
+		 * entityselector. 'item' is returned in case that the selector is for a new property in an
+		 * item.
 		 *
 		 * @private
 		 * @return {string|null}
 		 */
-		_getPropertyContext: function() {
+		_getPropertyContext: function () {
 			if ( this._isInNewStatementView() ) {
 				return 'item';
 			} else if ( this._isQualifier() ) {
@@ -151,7 +163,7 @@
 		 * @private
 		 * @return {boolean}
 		 */
-		_isQualifier: function() {
+		_isQualifier: function () {
 			var $statementview = this.element.closest( ':wikibase-statementview' ),
 				statementview = $statementview.data( 'statementview' );
 
@@ -166,7 +178,7 @@
 		 * @private
 		 * @return {boolean}
 		 */
-		_isReference: function() {
+		_isReference: function () {
 			var $referenceview = this.element.closest( ':wikibase-referenceview' );
 
 			return $referenceview.length > 0;
@@ -174,12 +186,15 @@
 
 		/**
 		 * detect if this is a new statement view.
+		 *
 		 * @private
 		 * @return {boolean}
 		 */
-		_isInNewStatementView: function() {
+		_isInNewStatementView: function () {
 			var $statementview = this.element.closest( ':wikibase-statementview' ),
-				value = $statementview.length > 0 ? $statementview.data( 'statementview' ).option( 'value' ) : null;
+				value = $statementview.length > 0
+					? $statementview.data( 'statementview' ).option( 'value' )
+					: null;
 
 			return !value;
 		}
